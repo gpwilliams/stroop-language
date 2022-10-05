21bd80a9bcd02c01755b43851b3a8aa11d96270f
Glenn Patrick Williams <33417082+gpwilliams@users.noreply.github.com>
2022-10-05 11:09:55 +0100

    Update read_csvs.R

--- R/00_load-functions/read_csvs.R
+++ R/00_load-functions/read_csvs.R
@@ -1,5 +1,23 @@
-read_csvs <- function(.file_path, .skip_first = TRUE, .id = "study") {
-  file_names <- list.files(path = .file_path)
+read_csvs <- function(
+    .file_path, 
+    .skip_first = TRUE, 
+    .id = "study", 
+    .include = NULL, # string
+    .exclude = NULL
+    ) {
+  #' Read all csvs in a directory into and bind together as a tibble. 
+  #' Crete a column identifying the data source with a custom column label.
+  #' Optionally skip the first column.
+  #' Subset the files to those in .include only or not in .exclude.
+  #' @return Tibble of merged csvs.
+  #' @param .file_path string: The file path to the folder containing the csvs.
+  #' @param .skip_first logical: Whether the first column should be excluded. 
+  #' This is useful for cases where row numbers are included in the csv but not needed.
+  #' @param .id string: the column name used to identify the data source (i.e. file).
+  #' @param .include string or NULL: keep only files containing the string.
+  #' @param .exclude string or NULL: remove any files containing the string.
+  #' @examples
+  #' read_csvs(here("path/to/file"), .exclude = "demographics")
   
   # decide if first column should be dropped or not
   if(.skip_first == TRUE) {
@@ -7,20 +25,48 @@ read_csvs <- function(.file_path, .skip_first = TRUE, .id = "study") {
   } else {
     skip_col <- NULL
   }
-  
-  # load all files in folder ending in .csv
-  dat <- list.files(
-    path = .file_path, 
-    full.names = TRUE,
-    pattern = "*.csv"
-  ) |> 
-    map(~read_csv(
-      ., 
-      show_col_types = FALSE, 
-      col_select = !skip_col
-    ))
-  
-  # set names
-  names(dat) <- sub("\\.csv$", "", file_names) 
-  bind_rows(dat, .id = .id)
+
+ # get paths for all files in folder ending in .csv
+ files <- list.files(
+  path = .file_path, 
+  full.names = TRUE,
+  pattern = "*.csv"
+  )
+ # get their names
+ file_names <- list.files(path = .file_path)
+ 
+ # keep only things in include
+ if(!is.null(.include)) {
+   files <- files[grepl(.include, files)]
+   file_names <- file_names[grepl(.include, file_names)]
+   }
+
+ # remove things in exclude
+ if(!is.null(.exclude)) {
+   files <- files[!grepl(.exclude, files)]
+   file_names <- file_names[!grepl(.exclude, file_names)]
+   }
+ 
+  if(length(files) > 0) {
+    suppressMessages(
+      dat <- files |> 
+        map(~read_csv(
+          ., 
+          col_types = cols(.default = "c"),
+          show_col_types = FALSE, 
+          col_select = !skip_col
+        ))
+    )
+    # set names
+    names(dat) <- sub("\\.csv$", "", file_names) 
+    bind_rows(dat, .id = .id) |> 
+      type_convert()
+  } else {
+    stop(
+     paste(
+       "No files match the inputted paramaters.",
+       "Did you provide the same pattern to include and exclude?"
+       )
+    )
+  }
 }
